039a80deb8ff27b20d23a1b8468801ec4d3e24c8
Tulasi <tulsishuklag@gmail.com>
2026-07-25 21:04:05 +0530

    backend/app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/auth.routes";
+import leadRoutes from "./routes/lead.routes";
 
 const app = express();
 
@@ -12,5 +14,10 @@ app.get("/", (req, res) => {
     message: "Lead Management API Running"
   });
 });
+app.use("/api/auth", authRoutes);
+app.use("/api/leads", leadRoutes);
+
+
 
 export default app;
+
